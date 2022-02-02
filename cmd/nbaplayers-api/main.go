fa659460a59d2a1cbd45bd4dc9e92bc4ee7ad0fc
OctavianUrsu <phenomenno@gmail.com>
2022-02-02 20:19:58 +0200

    Create HTTP Server and serve it

--- cmd/nbaplayers-api/main.go
+++ cmd/nbaplayers-api/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net/http"
 
+	"github.com/OctavianUrsu/go-nbaplayers-api/internal/players"
+	"github.com/OctavianUrsu/go-nbaplayers-api/internal/teams"
+)
+
+const PORT string = ":8080"
+
+func main() {
+	// Route and handle home page
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/players", players.ReturnAllPlayers)
+	http.HandleFunc("/teams", teams.ReturnAllTeams)
+
+	// Build HTTP Server
+	log.Fatal(http.ListenAndServe(PORT, nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Home of NBA Players API")
 }
