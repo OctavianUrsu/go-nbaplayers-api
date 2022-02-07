b58dcb8c19fa8d3fab49da2f29c0f2d3eae0fd8d
OctavianUrsu <phenomenno@gmail.com>
2022-02-07 17:25:58 +0200

    Add logging middleware to req/resp calls

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	mw "github.com/OctavianUrsu/go-nbaplayers-api/internal/middleware"
 	"github.com/OctavianUrsu/go-nbaplayers-api/internal/service"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/sirupsen/logrus"
 )
 
 // class
@@ -16,7 +19,13 @@ func NewHandler(ps *service.PlayerService) *Handler {
 
 // method
 func (h *Handler) InitRoutes() chi.Router {
+	logger := logrus.New()
+
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(mw.NewStructuredLogger(logger))
+	r.Use(middleware.Recoverer)
 
 	r.Route("/api", func(r chi.Router) {
 		r.Route("/players", func(r chi.Router) {
---
+++ internal/middleware/structuredLogger.go
@@ -0,0 +1,55 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/sirupsen/logrus"
+)
+
+// LOGGER
+type StructuredLogger struct {
+	Logger *logrus.Logger
+}
+
+type StructuredLoggerEntry struct {
+	Logger logrus.FieldLogger
+}
+
+func NewStructuredLogger(logger *logrus.Logger) func(next http.Handler) http.Handler {
+	return middleware.RequestLogger(&StructuredLogger{logger})
+}
+
+func (l *StructuredLogger) NewLogEntry(r *http.Request) middleware.LogEntry {
+	entry := &StructuredLoggerEntry{Logger: logrus.NewEntry(l.Logger)}
+	logFields := logrus.Fields{
+		"ts":     time.Now().UTC().Format(time.RFC1123),
+		"method": r.Method,
+		"path":   r.RequestURI,
+	}
+
+	entry.Logger = entry.Logger.WithFields(logFields)
+
+	entry.Logger.Infoln("request started")
+
+	return entry
+}
+
+func (l *StructuredLoggerEntry) Write(status, bytes int, header http.Header, elapsed time.Duration, extra interface{}) {
+	l.Logger = l.Logger.WithFields(logrus.Fields{
+		"status":     status,
+		"resp_bytes": bytes,
+		"resp_ms":    float64(elapsed.Milliseconds()),
+	})
+
+	l.Logger.Infoln("request complete")
+}
+
+func (l *StructuredLoggerEntry) Panic(v interface{}, stack []byte) {
+	l.Logger = l.Logger.WithFields(logrus.Fields{
+		"stack": string(stack),
+		"panic": fmt.Sprintf("%+v", v),
+	})
+}
