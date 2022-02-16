9e2f851c45b47642c303e076bbcf732b39cb6d2c
OctavianUrsu <phenomenno@gmail.com>
2022-02-16 14:01:56 +0200

    Add CORS

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 
 require (
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-chi/cors v1.2.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/OctavianUrsu/go-nbaplayers-api/pkg/service"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/sirupsen/logrus"
 )
 
@@ -28,6 +29,14 @@ func (h *Handler) InitRoutes() chi.Router {
 	r.Use(middleware.RequestID)
 	r.Use(mw.NewStructuredLogger(logger))
 	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://www.octavianursu.com/"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	// Initialize API routes
 	r.Route("/api", func(r chi.Router) {
