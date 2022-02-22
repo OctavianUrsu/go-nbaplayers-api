c32c4366ad1546f05b70b73cb39f5a0f589efff1
OctavianUrsu <phenomenno@gmail.com>
2022-02-22 02:15:53 +0200

    Prepare main for production build

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,6 @@ import (
 	"github.com/OctavianUrsu/go-nbaplayers-api/pkg/helpers"
 	"github.com/OctavianUrsu/go-nbaplayers-api/pkg/service"
 	"github.com/OctavianUrsu/go-nbaplayers-api/pkg/store"
-	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -23,12 +22,12 @@ func main() {
 	}
 
 	// Loading env variables
-	logrus.Infoln("Loading env variables...")
-	err := godotenv.Load(".env")
+	// logrus.Infoln("Loading env variables...")
+	// err := godotenv.Load(".env")
 
-	if err != nil {
-		logrus.Fatal("Error loading .env file")
-	}
+	// if err != nil {
+	// 	logrus.Fatal("Error loading .env file")
+	// }
 
 	port := os.Getenv("PORT")
 
